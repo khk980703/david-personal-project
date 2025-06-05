587f5f69724ca57690dc6ce2051b621bb2cf1eef
David Kim <khk980703@hotmail.com>
2025-06-05 15:07:08 +1200

    set up basic components

---
+++ client/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>TreadMe</h1>
+      <img
+        src="./public/images/Treadme.png"
+        alt="Buff Kiwi Logo"
+        className="logo"
+      />
+    </header>
+  )
+}
--- client/routes.tsx
+++ client/routes.tsx
@@ -1,7 +1,13 @@
 /* eslint-disable react/jsx-key */
 import { createRoutesFromElements, Route } from 'react-router'
+
+import Layout from './components/Layout'
 import App from './components/App'
 
-const routes = createRoutesFromElements(<Route index element={<App />} />)
+const routes = createRoutesFromElements(
+  <Route path="/" element={<Layout />}>
+    <Route index element={<App />} />
+  </Route>,
+)
 
 export default routes
